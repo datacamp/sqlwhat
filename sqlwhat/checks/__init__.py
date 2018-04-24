a008d03f024dfa051439b1d9811dc5a9032acc3b
Filip Schouwenaars <filip@datacamp.com>
2018-04-24 16:01:03 +0200

    import test_not from protowhat

--- sqlwhat/checks/__init__.py
+++ sqlwhat/checks/__init__.py
@@ -1,5 +1,5 @@
 from sqlwhat.checks.check_result import check_result, test_has_columns, test_nrows, test_ncols, test_column, allow_error, test_error, test_name_miscased, test_column_name, sort_rows
 
 from protowhat.checks.check_funcs import check_node, check_field, test_student_typed, has_equal_ast, verify_ast_parses
-from protowhat.checks.check_logic import fail, multi, extend, test_or, test_correct
+from protowhat.checks.check_logic import fail, multi, test_not, extend, test_or, test_correct
 from protowhat.checks.check_simple import test_mc, success_msg
