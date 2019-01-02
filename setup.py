6f0fbad504dd2fe5b4f72875147989cb83df6dde
Jeroen Hermans <jeroen@datacamp.com>
2019-01-02 21:12:44 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -5,27 +5,43 @@ import ast
 from os import path
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+PACKAGE_NAME = "sqlwhat"
+REQUIREMENT_NAMES = ["protowhat", "antlr-plsql", "antlr-tsql"]
 
-PACKAGE_NAME = 'sqlwhat'
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+VERSION_FILE = path.join(HERE, PACKAGE_NAME, "__init__.py")
+REQUIREMENTS_FILE = path.join(HERE, "requirements.txt")
+README_FILE = path.join(HERE, "README.md")
+
+with open(VERSION_FILE, encoding="utf-8") as fp:
+    _version_re = re.compile(r"__version__\s+=\s+(.*)")
+    VERSION = str(ast.literal_eval(_version_re.search(fp.read()).group(1)))
+with open(REQUIREMENTS_FILE, encoding="utf-8") as fp:
+    req_txt = fp.read()
+    _requirements_re_template = r"^({}(?:\s*[<>=]+\s*\S*)?)\s*(?:#.*)?$"
+    REQUIREMENTS = [
+        re.search(_requirements_re_template.format(requirement), req_txt, re.M).group(0)
+        for requirement in REQUIREMENT_NAMES
+    ]
+with open(README_FILE, encoding="utf-8") as fp:
     README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), 'rb') as fp:
-    VERSION = str(ast.literal_eval(_version_re.search(
-        fp.read().decode('utf-8')).group(1)))
 
 setup(
-	name=PACKAGE_NAME,
-	version=VERSION,
-	packages=['sqlwhat', 'sqlwhat.checks'],
-	install_requires=['protowhat', 'antlr-plsql>=0.1.0', 'antlr-tsql>=0.1.0'],
-        description = 'Submission correctness tests for SQL',
-        long_description=README,
-        long_description_content_type='text/markdown',
-        license='GNU version 3',
-        author='Michael Chow',
-        author_email='michael@datacamp.com',
-        maintainer='Filip Schouwenaars',
-        maintainer_email='filip@datacamp.com',
-        url = 'https://github.com/datacamp/sqlwhat')
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=[PACKAGE_NAME, "{}.checks".format(PACKAGE_NAME)],
+    install_requires=REQUIREMENTS,
+    description="Submission correctness tests for SQL",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Michael Chow, Filip Schouwenaars",
+    author_email="michael@datacamp.com",
+    maintainer="Jeroen Hermans",
+    maintainer_email="content-engineering@datacamp.com",
+    url="https://github.com/datacamp/sqlwhat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
