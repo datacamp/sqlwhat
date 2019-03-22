dbf655a8023c28c04f034d5388becc224dcda953
Jeroen Hermans <jeroen@datacamp.com>
2019-03-22 14:36:34 +0100

    Move has_ functions to separate file

--- sqlwhat/checks/__init__.py
+++ sqlwhat/checks/__init__.py
@@ -1,7 +1,6 @@
-from sqlwhat.checks.check_funcs import allow_error, has_no_error, \
-                                       has_result, has_nrows, has_ncols, \
-                                       check_column, check_row, check_all_columns, \
-                                       lowercase, has_equal_value, check_result, check_query
+from sqlwhat.checks.has_funcs import has_no_error, has_result, has_nrows, has_ncols, has_equal_value
+from sqlwhat.checks.check_funcs import allow_error, check_column, check_row, check_all_columns, \
+                                       lowercase, check_result, check_query
 
 # functions from protowhat, exposed in sqlwhat
 from protowhat.checks.check_funcs import check_node, check_edge, has_code, has_equal_ast, has_parsed_ast
