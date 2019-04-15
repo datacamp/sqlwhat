adff16707731fb388c058cf013e15b64dc4ea8ff
Jeroen Hermans <jeroen@datacamp.com>
2019-04-15 04:22:55 +0200

    Fix remaining formatting issues

--- sqlwhat/checks/__init__.py
+++ sqlwhat/checks/__init__.py
@@ -1,8 +1,27 @@
-from sqlwhat.checks.has_funcs import has_no_error, has_result, has_nrows, has_ncols, has_equal_value
-from sqlwhat.checks.check_funcs import allow_error, check_column, check_row, check_all_columns, \
-                                       lowercase, check_result, check_query
+from sqlwhat.checks.has_funcs import (
+    has_no_error,
+    has_result,
+    has_nrows,
+    has_ncols,
+    has_equal_value,
+)
+from sqlwhat.checks.check_funcs import (
+    allow_error,
+    check_column,
+    check_row,
+    check_all_columns,
+    lowercase,
+    check_result,
+    check_query,
+)
 
 # functions from protowhat, exposed in sqlwhat
-from protowhat.checks.check_funcs import check_node, check_edge, has_code, has_equal_ast, has_parsed_ast
+from protowhat.checks.check_funcs import (
+    check_node,
+    check_edge,
+    has_code,
+    has_equal_ast,
+    has_parsed_ast,
+)
 from protowhat.checks.check_logic import fail, multi, check_not, check_or, check_correct
 from protowhat.checks.check_simple import has_chosen, success_msg
