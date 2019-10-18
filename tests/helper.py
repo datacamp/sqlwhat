dc056375e77918cffa8503a9fd8861c68ba9f9fb
TimSangster <tim@datacamp.com>
2019-10-18 10:43:20 +0200

    Remove unused code

--- tests/helper.py
+++ tests/helper.py
@@ -28,18 +28,6 @@ def get_sct_payload(output):
         return None
 
 
-def run(data):
-    from sqlbackend.Exercise import Exercise
-
-    exercise = Exercise(data)
-    output = exercise.runInit()
-    if "backend-error" in str(output):
-        print(output)
-        raise (ValueError("Backend error"))
-    output = exercise.runSubmit(data)
-    return get_sct_payload(output)
-
-
 class Connection:
     """Mock up conn.dialect.name"""
 
